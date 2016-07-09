91f25fa3a84454961426c480245ee121519391d1
kelukelugames <kelukelugames@gmail.com>
2016-07-09 15:07:22 -0700

    Add documentation

--- lastWords.py
+++ lastWords.py
@@ -8,8 +8,10 @@ import threading
 from multiprocessing import Pool
 from urllib.request import urlopen
 
-"""
-  This is my first time writing in python. Feedback is welcome.
+"""Script to print the last comments of Hacker News users before they are banned.
+Reads from the Hacker News API: https://github.com/HackerNews/API)
+
+This is my first time writing python. Feedback is welcome.
 """
 
 # Global variables
@@ -22,6 +24,9 @@ _LOCK = threading.Lock()
 # Examples: Recently unbanned users or https://news.ycombinator.com/item?id=12051001
 # Smaller value misses the case for 10551997
 _BAN_THRESHOLD_SECONDS = 60 * 60 * 24 * 3
+_MOD_NAME = "dang"
+  # Increasing the number of threads beyond 8 on my machine causes Http errors.
+_MAX_NUM_THREADS = 8
 
 DEBUG = True
 
@@ -46,19 +51,34 @@ def __is_comment(data):
     and "time" in data
 
 def __is_possible_ban(data):
+  """Checks if mod wrote a comment to announce a ban.
+  Args:
+      data: json item from Hacker News.
+  Returns:
+      Returns true if the comment text contains the word "banned".
+  """
   return __is_comment(data) \
     and "banned" in data["text"] \
     and "parent" in data
 
 def __is_user_banned(user_comment, mod_comment):
-  #TODO: add doc
+  """Checks if the user is banned.
+
+  Checks the timestamp of the last submitted item from the user.
+  If the time is _BAN_THRESHOLD_SECONDS after the mod's comment then the user is still active.
+
+  Args:
+      user_comment: json item representing the user's comment.
+      mod_comment: json item representing the mod's comment
+  Returns:
+      Returns true if the user is banned.
+  """
   if __is_comment(user_comment) is False:
     return False  
 
   user_data = __get_user(user_comment["by"])
   recent_comment = __get_comment(user_data["submitted"][0])
 
-  # if mod comments and then bans then user, we might miss a few banned users due to race condition.
   if DEBUG:
     print("Time delta: " + str(mod_comment["time"] - recent_comment["time"]))
 
@@ -77,18 +97,20 @@ def __write_to_file(comment):
     _LOCK.release()
 
 def __process_item(id):
+  """Checks if the parent user is banned and writes their last comment to file.
+  Determins if the mod's comment is a ban announcment.
+  If yes then check the user to see if they are banned.
+  Write the user's last comment to file if they are banned.
+  Last comment is defined as the parent comment of the mod's comment,
+  which might not be the last submitted item from the user.
+
+  Args:
+      id: Id for Hacker News API.
+  """
   if DEBUG:
     print("Processing: " + str(id))
 
-  mod_comment = ""
-  try:
-    mod_comment = json.load(_DECODER(urlopen("https://hacker-news.firebaseio.com/v0/item/"
-      + str(id)
-      + ".json",
-      context=_CONTEXT)))
-  except http.client.BadStatusLine:
-    print("failed: " + str(id))
-    return 0
+  mod_comment = __get_comment(id)
 
   if __is_possible_ban(mod_comment):
     parent_comment = __get_comment(mod_comment["parent"])
@@ -96,19 +118,21 @@ def __process_item(id):
     if __is_user_banned(parent_comment, mod_comment):
       __write_to_file(parent_comment)
     return 1
+
   return 0
 
 def run_job():
-  #TODO: add doc
-  mod_data = __get_user("dang")
+  """Main function of the script
+  Gets Id of all comments of the mod. Use a thread pool to fetch the json data for each Id.
+  Then check if item is related to a banned user.
+  """
+  mod_data = __get_user(_MOD_NAME)
   submit_ids = mod_data["submitted"]
+  # Uncomment for test data.
+  submit_ids = [10551997, 7867166, 12041458, 12059888, 11631519]
 
-  # Uncomment for test data
-  #submit_ids = [10551997, 7867166, 12041458, 12059888, 11631519]
-
-  pool = Pool(8)
+  pool = Pool(_MAX_NUM_THREADS)
   results = pool.map(__process_item, submit_ids)
-
   pool.close()
   pool.join()
 
@@ -116,4 +140,3 @@ def run_job():
   print("Total banned users detected: " + str(sum(results)))
 
 run_job()
-
