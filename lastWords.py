9620ba63b2f90999883dd912b3b97290f61684d3
kelukelugames <kelukelugames@gmail.com>
2016-07-09 09:04:39 -0700

    Change to spaces

--- lastWords.py
+++ lastWords.py
@@ -11,66 +11,64 @@ import http.client
 
 import codecs
 
+"""
+Global variables
+"""
+
 decoder = codecs.getreader("utf-8")
-
-# This restores the same behavior as before.
+# To prevent SSL failure after too many calls.
 context = ssl._create_unverified_context()
-
 lock = threading.Lock()
-
-
-
 modFile = open("modComments.txt", "w")
-
 modData = json.load(decoder(urlopen("https://hacker-news.firebaseio.com/v0/user/dang.json", context=context)))
-#submitIds = [10551997]
-#[10920581,10855519,10792592,10320514,10976819,10442063,10282401,10658971,11083321,10397703,10606144,10729695,11044820,10358171,10482064,10551997,10920550,10792570,10320507,10854886,10976799,10442059,10281892]
 submitIds = modData["submitted"]
+
+submitIds = [10551997, 7867166, 12041458, 12059888]
 
 print(len(submitIds))
 
 for id in submitIds:
-	if type(id) != type(3):
-		print(type(id))
+  if type(id) != type(3):
+    print(type(id))
 
 def isBanningComment(data, id):
-	print(id)
-	return data is not None \
-		and "type" in data \
-		and data["type"] == "comment" \
-		and "text" in data \
-		and "banned" in data["text"] \
-		and "parent" in data
+  print(id)
+  return data is not None \
+    and "type" in data \
+    and data["type"] == "comment" \
+    and "text" in data \
+    and "banned" in data["text"] \
+    and "parent" in data
 
 def foo(id):
-	modComment = ""
-	try:
-		modComment = json.load(decoder(urlopen("https://hacker-news.firebaseio.com/v0/item/"
-			+ str(id)
-			+ ".json",
-			context=context)))
-	except http.client.BadStatusLine:
-		print("***********************************************")
-		print("failed: " + str(id))
-		print("***********************************************")
-		return 0
+  modComment = ""
+  try:
+    modComment = json.load(decoder(urlopen("https://hacker-news.firebaseio.com/v0/item/"
+      + str(id)
+      + ".json",
+      context=context)))
+  except http.client.BadStatusLine:
+    print("***********************************************")
+    print("failed: " + str(id))
+    print("***********************************************")
+    return 0
 
-	if isBanningComment(modComment, id):
+  if isBanningComment(modComment, id):
 
-		lock.acquire()
-		#next check if user is banned
-		print("id: " + str(modComment['id']))
+    lock.acquire()
+    #next check if user is banned
+    print("id: " + str(modComment['id']))
 
-		modCommentText = modComment["text"]
-		#modCommentText = modCommentText.encode('utf-8')
+    modCommentText = modComment["text"]
+    #modCommentText = modCommentText.encode('utf-8')
 
-		#print("banned text: " + modCommentText)
-		modFile.write(modCommentText)
-		modFile.write("\n\n")
-		lock.release()
-		return 1
-	else:
-		return 0
+    #print("banned text: " + modCommentText)
+    modFile.write(modCommentText)
+    modFile.write("\n\n")
+    lock.release()
+    return 1
+  else:
+    return 0
 
 pool = Pool(8)
 results = pool.map(foo, submitIds)
