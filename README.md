e8c30e19ad667a50e8f681000066bcd01a3dea93
kelukelugames <kelukelugames@gmail.com>
2016-07-09 12:34:05 -0700

    remove escape characters

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
-*"To the 66% of you who are obese or overweight: put down the fork. It can be done.
-"*  [--Girlang](https://news.ycombinator.com/user?id=Girlang)
+*"Again, hate to be a hater on this guy, but I can't take anything seriously after "the blog post" about 42 Floors interview process: <a href="http://web.archive.org/web/20140618142018/http://blog.42floors.com/interviewing-at-a-startup/" rel="nofollow">http://web.archive.org/web/20140618142018/http://blog.42floo...</a><p>And that domain name - HumbledMBA.com? Sounds super humble.<p>The whole blog looks like fodder for the next season of Silicon Valley."*
+  [--42floorshater](https://news.ycombinator.com/user?id=42floorshater)
 
 
-*"You might <i></i>not<i></i> want to take parental advice from a fat bastard who sits and plays with his keyboard all day on a computer.<p>Try a grown-up instead. By which I mean, someone who has chosen to develop emotional intelligence.<p>Yes I can see the irony in what I&#x27;m saying. Thanks.
-"*  [--throwaway6436](https://news.ycombinator.com/user?id=throwaway6436)
+*"To the 66% of you who are obese or overweight: put down the fork. It can be done."*
+  [--Girlang](https://news.ycombinator.com/user?id=Girlang)
+
+
+*"You might <i></i>not<i></i> want to take parental advice from a fat bastard who sits and plays with his keyboard all day on a computer.<p>Try a grown-up instead. By which I mean, someone who has chosen to develop emotional intelligence.<p>Yes I can see the irony in what I'm saying. Thanks."*
+  [--throwaway6436](https://news.ycombinator.com/user?id=throwaway6436)
 
 
--- lastWords.py
+++ lastWords.py
@@ -1,30 +1,29 @@
 
+import codecs
+import http.client
 import json
+import ssl
 import threading
-#import urllib2
 
 from multiprocessing import Pool
 from urllib.request import urlopen
 
-import ssl
-import http.client
-
-import codecs
-
 """
-Global variables
+  This is my first time writing in python. Feedback is welcome.
 """
 
+# Global variables
 _DECODER = codecs.getreader("utf-8")
 # To prevent SSL failure after too many calls.
 _CONTEXT = ssl._create_unverified_context()
 _LOCK = threading.Lock()
-# 3 days
+# _BAN_THRESHOLD_SECONDS = 3 days
+# Bigger values increases false positives. 
+# Examples: Recently unbanned users or https://news.ycombinator.com/item?id=12051001
+# Smaller value misses the case for 10551997
 _BAN_THRESHOLD_SECONDS = 60 * 60 * 24 * 3
 
 DEBUG = True
-
-
 
 def __get_user(id):
   return json.load(_DECODER(urlopen("https://hacker-news.firebaseio.com/v0/user/"
@@ -52,6 +51,7 @@ def __is_possible_ban(data):
     and "parent" in data
 
 def __is_user_banned(user_comment, mod_comment):
+  #TODO: add doc
   if __is_comment(user_comment) is False:
     return False  
 
@@ -60,8 +60,21 @@ def __is_user_banned(user_comment, mod_comment):
 
   # if mod comments and then bans then user, we might miss a few banned users due to race condition.
   if DEBUG:
-    print(mod_comment["time"] - recent_comment["time"])
+    print("Time delta: " + str(mod_comment["time"] - recent_comment["time"]))
+
   return mod_comment["time"] > recent_comment["time"] - _BAN_THRESHOLD_SECONDS
+
+def __write_to_file(comment):
+    _LOCK.acquire()
+    with open("README.md", 'a') as out:
+      # remove escape characters
+      comment_text = comment["text"].replace("&#x2F;", "/")
+      comment_text = comment_text.replace("&#x27;", "'")
+      comment_text = comment_text.replace("&quot;", "\"")
+
+      out.write("*\"" + comment_text + "\"*" + "\n")
+      out.write("  [--" + comment["by"] + "](https://news.ycombinator.com/user?id=" + comment["by"] + ")\n\n\n")
+    _LOCK.release()
 
 def __process_item(id):
   if DEBUG:
@@ -74,31 +87,26 @@ def __process_item(id):
       + ".json",
       context=_CONTEXT)))
   except http.client.BadStatusLine:
-    print("***********************************************")
     print("failed: " + str(id))
-    print("***********************************************")
     return 0
 
   if __is_possible_ban(mod_comment):
     parent_comment = __get_comment(mod_comment["parent"])
 
     if __is_user_banned(parent_comment, mod_comment):
-      _LOCK.acquire()
-      with open("README.md", 'a') as out:
-        out.write("*\"" + parent_comment["text"] + "\n" + "\"*" )
-        out.write("  [--" + parent_comment["by"] + "](https://news.ycombinator.com/user?id=" + parent_comment["by"] + ")\n\n\n")
-      _LOCK.release()
+      __write_to_file(parent_comment)
     return 1
   return 0
 
 def run_job():
+  #TODO: add doc
   mod_data = __get_user("dang")
   submit_ids = mod_data["submitted"]
 
   # Uncomment for test data
-  submit_ids = [10551997, 7867166, 12041458, 12059888]
+  submit_ids = [10551997, 7867166, 12041458, 12059888, 11631519]
 
-  pool = Pool(1)
+  pool = Pool(8)
   results = pool.map(__process_item, submit_ids)
 
   pool.close()
